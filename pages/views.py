fddbe490561667f4f87200eaadcb7b3e69df47f7
SmokeyWen <835033838wen@gmail.com>
2020-09-08 00:54:58 -0700

    finished building and routing for individual listing

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from authors.models import Author
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-complete_date')[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    authors = Author.objects.order_by('id')
+    context = {
+        'authors': authors
+    }
+    return render(request, 'pages/about.html', context)
