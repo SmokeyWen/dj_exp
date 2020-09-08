dbc3b9f80e008b561b69248e01025b23ea3ee48e
SmokeyWen <835033838wen@gmail.com>
2020-09-08 02:54:38 -0700

    independent search page done, connecting to index.html. search function done.

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
+from listings.choices import author_choice, price_choice, duration_choice
 
 # Create your views here.
 def index(request):
@@ -23,4 +24,39 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    query_list = Listing.objects.order_by('-complete_date')
+
+    #filter with title keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_list = query_list.filter(title__icontains = keywords)
+
+    #filter with author dropdown
+    #searching with foreign key of the listing table
+    if 'author' in request.GET:
+        author = request.GET['author']
+        if author:
+            query_list = query_list.filter(author__name__icontains = author)
+
+    #filter with initial price. naming changed between select and db column name
+    if 'price' in request.GET:
+        initial_price = request.GET['price']
+        if initial_price:
+            query_list = query_list.filter(initial_price__lte = initial_price)
+
+    #filter with number of days. naming changed with db column name
+    if 'days' in request.GET:
+        duration = request.GET['days']
+        if duration:
+            query_list = query_list.filter(duration__lte = duration)
+
+
+
+    context = {
+        'author_choice': author_choice,
+        'price_choice': price_choice,
+        'duration_choice': duration_choice,
+        'results': query_list
+    }
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from authors.models import Author
+from listings.choices import author_choice, price_choice, duration_choice
 
 def index(request):
     listings = Listing.objects.order_by('-complete_date')[:3]
     context = {
-        'listings': listings
+        'listings': listings,
+        'author_choice': author_choice,
+        'price_choice': price_choice,
+        'duration_choice': duration_choice
     }
     return render(request, 'pages/index.html', context)
 
