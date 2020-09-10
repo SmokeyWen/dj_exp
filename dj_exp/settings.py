f67341918be7cd4865e7b966f46995fd1300e743
SmokeyWen <835033838wen@gmail.com>
2020-09-10 14:40:07 -0700

    hid email credentials

--- dj_exp/settings.py
+++ dj_exp/settings.py
@@ -141,11 +141,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'painkeybywen@gmail.com'
-EMAIL_HOST_PASSWORD = 'yskzaqdmpuryzjsr'
-EMAIL_USE_TLS = True
+
 
 try:
     from .local_settings import *
